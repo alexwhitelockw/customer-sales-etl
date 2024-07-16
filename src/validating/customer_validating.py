7866a3edf572645649954306525985ad806656f3
alexwhitelockw <alexwhitelockw@gmail.com>
2024-07-16 11:24:26 +0100

    Updated documentation

--- src/validating/customer_validating.py
+++ src/validating/customer_validating.py
@@ -6,7 +6,7 @@ from src.utils.utils import validate_customer_id
 if __name__ == "__main__":
     customer_details = pd.read_csv("data/transformed/customer_details.csv")
 
-    # Check customer ID format to ensure it aligns with standard
+    # Check customer ID format to ensure it aligns with the standard
     validate_customer_id(customer_details, "customer_id")
 
     customer_details.to_csv("data/validated/customer_details.csv", index=False)
